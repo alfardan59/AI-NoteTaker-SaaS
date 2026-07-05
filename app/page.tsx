a5356925e3b2c70e891f01d563d0a72c0c702968
alfardan <alfardanarfin500@gmail.com>
2026-07-05 18:07:05 +0530

    feat: add IntegrationsSection component with integration logos and update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/landing/HeroSection";
 import FeaturesSection from "./components/landing/FeaturesSection";
+import IntegrationsSection from "./components/landing/IntegrationsSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <HeroSection />
       <FeaturesSection />
+      <IntegrationsSection />
     </div>
   );
 }
